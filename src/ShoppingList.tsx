1d4cbc54bcc113016e727276acfa159634812e85
Paul Adams <ghusiyy@gmail.com>
2024-03-19 23:58:35 -0700

    add ShoppingListItem type and state

--- src/ShoppingList.tsx
+++ src/ShoppingList.tsx
@@ -1,6 +1,16 @@
+import { useState } from "react";
 import "./ShoppingList.css";
 
+interface ShoppingListItem {
+  name: string;
+  quantity: number;
+  isPurchased: boolean;
+}
+
 function ShoppingList() {
+  const [shoppingListItems, setShoppingListItems] = useState<
+    ShoppingListItem[]
+  >([]);
   return <div className="shopping-list-wrapper">{/* TODO: implement */}</div>;
 }
 
