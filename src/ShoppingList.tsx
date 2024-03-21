e7dbbfe4db7e0b63add6cc22085526a0a28c8b4f
Paul Adams <ghusiyy@gmail.com>
2024-03-20 23:54:41 -0700

    add ability to add  and render new shopping items

--- src/ShoppingList.tsx
+++ src/ShoppingList.tsx
@@ -1,7 +1,9 @@
 import { useState } from "react";
+import { nanoid } from "nanoid";
 import "./ShoppingList.css";
 
 interface ShoppingListItem {
+  id?: string;
   name: string;
   quantity: number;
   isPurchased: boolean;
@@ -11,7 +13,44 @@ function ShoppingList() {
   const [shoppingListItems, setShoppingListItems] = useState<
     ShoppingListItem[]
   >([]);
-  return <div className="shopping-list-wrapper">{/* TODO: implement */}</div>;
+  const [newItem, setNewItem] = useState<ShoppingListItem>({
+    id: nanoid(),
+    name: "",
+    quantity: 1,
+    isPurchased: false,
+  });
+  function addNewItem() {
+    setShoppingListItems((items) => [...items, newItem]);
+  }
+  return (
+    <div className="shopping-list-wrapper">
+      <input
+        value={newItem?.name}
+        onChange={(e) =>
+          setNewItem((prevItem) => ({ ...prevItem, name: e.target.value }))
+        }
+      />
+      <input
+        min={1}
+        type="number"
+        value={newItem?.quantity}
+        onChange={(e) =>
+          setNewItem((prevItem) => ({
+            ...prevItem,
+            quantity: Number(e.target.value),
+          }))
+        }
+      />
+      <button onClick={() => addNewItem()}>Add</button>
+      <ul>
+        {shoppingListItems.map((item) => (
+          <li key={item.id}>
+            {item.name} - {item.quantity}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default ShoppingList;
